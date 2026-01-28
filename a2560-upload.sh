d14ccfb2d9bab2509e29840489c1dc43277b068e
Scott Giese <scttgs0@gmail.com>
2026-01-28 16:04:10 -0600

    use m68040 processor

---
+++ a2560-upload.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+c256mgr --binary emutos-a2560k.rom --address 0x100000
--- make_a2560k_flash.sh
+++ make_a2560k_flash.sh
@@ -1,6 +1,6 @@
 # change "define ROM_ORIGIN 0x00e00000" if needed
 make clean;make -f Makefile.ld clean
-make a2560u UNIQUE=fr
+make a2560k COUNTRY=us
 make -f Makefile.ld
-make a2560u UNIQUE=fr
-then use "upload-to-flash" script
+make a2560k CPUNTRY=us
+# then use "upload-to-flash.sh" script
